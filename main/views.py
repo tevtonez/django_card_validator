3c3b42d1909d4dd689b927509377790ce753b2d7
tevtonez <tevtonez@gmail.com>
2018-02-26 10:16:17 +0100

    add form to hp

--- main/views.py
+++ main/views.py
@@ -2,17 +2,23 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 
 from django.urls import reverse_lazy
-from django.views.generic import View, TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import FormView, View, TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from django.contrib.auth.decorators import login_required
 
-from . import forms
-from . import models
+from main.forms import CardValidatorForm
 
 
-class IndexView(TemplateView):
+class IndexView(FormView):
     template_name = 'index.html'
+    form_class = CardValidatorForm
+    success_url = reverse_lazy('home')
 
+    def get_context_data(self, **kwargs):
+        print("processing get")
+        context = super(IndexView, self).get_context_data(**kwargs)
+        return context
 
-# class mainIndexView(TemplateView):
-#     template_name = 'main/base.html'
+    def form_valid(self, form):
+        print(form.cleaned_data['card_number'])
+        return super(IndexView, self).form_valid(form)
