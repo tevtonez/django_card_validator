d4201e9c2e70d2b445cec7950215229713b19603
tevtonez <tevtonez@gmail.com>
2018-02-27 18:00:37 +0100

    add unhappy path unittest: wrong card number; add validation to views

--- main/tests.py
+++ main/tests.py
@@ -11,6 +11,7 @@ class TestApiEndpoint(TestCase):
         self.client = Client()
         self.valid_card_number = '349489219405037'
         self.invalid_card_number = '349489219405039'
+        self.chars_number = "abc234543636"
 
     def test_correct_num(self):
         """Happy path: card number is valid."""
@@ -40,4 +41,19 @@ class TestApiEndpoint(TestCase):
         self.assertEqual(
             response_dict['result'],
             'Entered card number is Invalid!'
+        )
+
+    def test_incorrect_input(self):
+        """Unhappy path: incorrect input - non-integers."""
+        self.response = self.client.post('/api/check_number/', {
+            'card_number': self.chars_number
+        })
+
+        response_dict = json.loads(self.response.content.decode("utf-8"))
+
+        self.assertEqual(self.response.status_code, 200)
+        self.assertEqual(response_dict['result_class'], 'danger')
+        self.assertEqual(
+            response_dict['result'],
+            "Please enter digits only."
         )
