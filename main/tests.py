ddb73b762be201c287342c66a1e74e0be4645baf
tevtonez <tevtonez@gmail.com>
2018-02-27 17:32:30 +0100

    add happypath unittest

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+"""Unit tests for card validation number functionality."""
+from django.test import TestCase, Client
+import json
 
-# Create your tests here.
+
+class TestApiEndpoint(TestCase):
+    """Testing card number validation endpoint."""
+
+    def setUp(self):
+        """Test setup."""
+        self.client = Client()
+        self.valid_card_number = '349489219405037'
+        self.invalid_card_number = '349489219405039'
+
+    def test_correct_num(self):
+        """Happy path: card number is valid."""
+        self.response = self.client.post('/api/check_number/', {
+            'card_number': self.valid_card_number
+        })
+
+        response_dict = json.loads(self.response.content.decode("utf-8"))
+
+        self.assertEqual(self.response.status_code, 200)
+        self.assertEqual(response_dict['result_class'], 'success')
+        self.assertEqual(
+            response_dict['result'],
+            'Your card number is Valid.'
+        )
